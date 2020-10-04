7772b8085b1fbd427c21492a88381538918c61ae
Nikhilananddev <58386262+Nikhilananddev@users.noreply.github.com>
2020-10-04 21:28:28 +0530

    Update and rename class.js to  Vehicle.js

---
+++ Vehicle.js
@@ -0,0 +1,11 @@
+class  Vehicle  {
+
+  constructor(brand ,price,color,model ) {
+    this.carname = brand;
+    this.carcolor carcolor;
+    this.price price;
+    this.model=model;
+  
+  }
+}
+car1 = new Car("Ford",500,"Red","008x5");
--- class.js
+++
@@ -1,6 +0,0 @@
-class Car {
-  constructor(brand) {
-    this.carname = brand;
-  }
-}
-mycar = new Car("Ford");
